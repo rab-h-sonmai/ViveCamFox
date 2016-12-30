4de7ab9187cfda82021aacc1d7a246be1ea14780
rab-h-sonmai <rab.h.sonmai@gmail.com>
2016-12-30 20:22:33 +0100

    Initial commit - just the basics working, no real test scene to speak of. TODO: add fox to justify oh so witty title. HEY, it's been a crappy time, alright?

---
+++ Assets/Scripts/ViveCameraTexture.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveCameraTexture : MonoBehaviour {
+
+    public Texture2D Texture
+    {
+        get { return m_texture; }
+    }
+
+    private Texture2D m_texture = null;
+    private IntPtr m_frameBuffer = IntPtr.Zero;
+    private uint m_frameBufferSize = 0;
+    private CVRTrackedCamera m_camera = null;
+    private ulong m_streamHandle = 0;
+    private CameraVideoStreamFrameHeader_t m_frameHeader;
+    private uint m_frameHeaderSize = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_camera = OpenVR.TrackedCamera;
+        if (m_camera == null)
+        {
+            Debug.LogError("No camera found");
+            return;
+        }
+
+        // First get the size of a frame
+        uint width = 0;
+        uint height = 0;
+        uint bufferSize = 0;
+        EVRTrackedCameraError cameraError = m_camera.GetCameraFrameSize(0, EVRTrackedCameraFrameType.Undistorted, ref width, ref height, ref bufferSize);
+        if (cameraError != EVRTrackedCameraError.None)
+        {
+            Debug.LogError("Could not get frame size (error=" + cameraError + ")");
+            return;
+        }
+
+        if (width * height == 0)
+        {
+            Debug.LogError("Frame size of 0, are you sure you've enabled the camera in the SteamVR settings panel?");
+            return;
+        }
+
+        uint bitsPerPixel = bufferSize / (width * height);
+        m_frameBufferSize = bufferSize;
+
+        // Then get a handle to the stream
+        cameraError = m_camera.AcquireVideoStreamingService(0, ref m_streamHandle);
+        if (cameraError == EVRTrackedCameraError.None)
+        {
+            m_frameBuffer = Marshal.AllocHGlobal((int)bufferSize);
+            m_frameHeader = new CameraVideoStreamFrameHeader_t();
+            m_frameHeaderSize = (uint)Marshal.SizeOf(m_frameHeader);
+
+            if (bitsPerPixel == 3)
+                m_texture = new Texture2D((int)width, (int)height, TextureFormat.RGB24, false);
+            else if (bitsPerPixel == 4)
+                m_texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false);
+            // if there's some other texture format here, we simply won't be able to do anything in Update()
+
+            if (m_texture && GetComponent<Renderer>())
+                GetComponent<Renderer>().material.mainTexture = m_texture;
+        }
+        else
+        {
+            Debug.LogError("Could not acquire handle to stream (error=" + cameraError + ")");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (m_streamHandle != 0)
+            m_camera.ReleaseVideoStreamingService(m_streamHandle);
+
+        if (m_frameBuffer != IntPtr.Zero)
+            Marshal.FreeHGlobal(m_frameBuffer);
+    }
+
+    void Update()
+    {
+        if (!m_texture)
+            return;
+
+        Valve.VR.EVRTrackedCameraError cameraError = m_camera.GetVideoStreamFrameBuffer(m_streamHandle, Valve.VR.EVRTrackedCameraFrameType.Undistorted, m_frameBuffer, m_frameBufferSize, ref m_frameHeader, m_frameHeaderSize);
+        // Check for an error here Debug.Log("STREAM: error=" + cameraError);
+        m_texture.LoadRawTextureData(m_frameBuffer, (int)m_frameBufferSize);
+        m_texture.Apply();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ViveCamFox
-Foxxy Vive Camera texture utility script
+# Vive Camera texture utility script
+
+### Assets you care about:
+- Assets\Scripts\ViveCameraTexture.cs
+  - The only important thing here, this tries to open a video stream and populate a texture with it, exposed as a property called "Texture". It also tries to set the texture of the object it's attached to, if there is a material available.
+- Assets\Materials\UpsideDown
+   - This is simply an unlit material with a -1 vertical scale, because the frame coming from the camera is upside-down
+
+The rest of the stuff in the repository is simply SteamVR gunk.
+
+### Points you should care about:
+- Don't forget to enable the camera first in the SteamVR Settings panel.
+- You may need to drop the refresh rate from 60 down to e.g., 30 if you are experiencing tracking problems.
